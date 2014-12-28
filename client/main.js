6768c437034c64966a108331fb9ef070a6192825
Philip Posgate <philip.posgate@gmail.com>
2014-12-28 17:31:19 -0500

    more stuff

--- client/main.js
+++ client/main.js
@@ -1,15 +1,11 @@
 Router.configure({
-  layoutTemplate: 'siteLayout',
-  load: function() {
-    // $('html, body').animate({scrollTop: 0}, 400);
-    // $('.content').hide().fadeIn(800);
-  }
+  layoutTemplate: 'siteLayout'
 });
 
 Router.route('/', function () {
   this.render('home');
 });
+
 Router.route('/home');
 Router.route('/about');
-Router.route('/contact');
 
